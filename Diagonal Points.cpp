32fae1ce77b2ac1e5969d5d583e8e1759ae5ace6
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-02-18 05:50:12 +0200

    Update Diagonal Points.cpp

--- Diagonal Points.cpp
+++ Diagonal Points.cpp
@@ -1,12 +1,14 @@
 #include <glut.h>
-float p1 = 0, p2 = 0, p3x = 0, p3y = 200, p4x = 0, p4y = 200, deltaP1 = 0.1, deltaP2 = 0.02, deltaP3 = 0.1, deltaP4 = 0.02;
+#include <stdlib.h>
+float p1 = 0, p2 = 0, p3x = 0, p3y = 200, p4x = 0, p4y = 200, deltaP1 = 0.1, deltaP2 = 0.02, deltaP3 = 0.1, deltaP4 = 0.02, c = 0;
+bool stop = false;
 void display() {
-	glClearColor(0, 0, 0, 1);
+	glClearColor(c, c, c, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glEnable(GL_POINT_SMOOTH);
 	glPointSize(8);
 	glBegin(GL_POINTS);
-	glColor3f(0.5, 0.5 , 1);
+	glColor3f(0.5, 0.5, 1);
 	glVertex2f(p1, p1);
 	glColor3f(0.5, 1, 0.5);
 	glVertex2f(p2, p2);
@@ -31,6 +33,18 @@ void display() {
 	p4y -= deltaP4;
 	if (p4x > 200 || p4x < 0 || p4y > 200 || p4y < 0)
 		deltaP4 = -deltaP4;
+	if (stop == false)
+		glutPostRedisplay();
+}
+void keyboard(unsigned char key, int x, int y) {
+	if (key == 27)  //Esc key
+		exit(0);   //successful program has been executed without any error or interrupt.
+	else if (key == 'c')
+		c = !c;		//change background color
+	else if (key == 'a')
+		stop = !stop;     //stop & resume
+	else if (key == 'r') 
+		p1 = 0, p2 = 0, p3x = 0, p3y = 200, p4x = 0, p4y = 200;    //reset points
 	glutPostRedisplay();
 }
 void main() {
@@ -38,6 +52,7 @@ void main() {
 	glutInitWindowSize(400, 400);
 	glutCreateWindow("Digonal Points");
 	gluOrtho2D(0, 200, 0, 200);
+	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
